a1ddc34681ed31183330e97ef47f36a9342bca97
menduong <bindhuongcong@gmail.com>
2021-03-09 16:11:25 +0700

    Polkafoundry docker test

--- docker/build.sh
+++ docker/build.sh
@@ -9,7 +9,7 @@ cd $PROJECT_ROOT
 
 # Find the current version from Cargo.toml
 VERSION=`grep "^version" ./Cargo.toml | egrep -o "([0-9\.]+)"`
-GITUSER=parity
+GITUSER=polkafoundry
 GITREPO=polkadot
 
 # Build the image
